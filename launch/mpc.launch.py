ee42eb3b8954097b3ad268d6f2723834ab984e73
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2023-10-10 23:58:34 +0300

    Add namespace argument to mpc.launch.py

--- launch/mpc.launch.py
+++ launch/mpc.launch.py
@@ -23,17 +23,26 @@ def generate_launch_description():
         description='Path to the YAML file with parameters for the mpc_node'
     )
 
+    # Declare the argument for the YAML file path
+    mpc_namespace_arg = DeclareLaunchArgument(
+        'mpc_namespace',
+        default_value='',
+        description='Namespace of the mpc_node'
+    )
+
     # Define the node
     mpc_node = Node(
         package=pkg_name,
         executable='mpc_node',
         name='mpc_node',
+        namespace=LaunchConfiguration('mpc_namespace'),
         output='screen',
         parameters=[LaunchConfiguration('yaml_path')],
         remappings=[
             ('px4_ros/in/odom', 'mavros/local_position/odom'),
             ('px4_ros/in/imu', 'mavros/imu'),
             ('traj_predictor/in/ref_traj', 'traj_predictor/ref_traj'),
+            ('traj_predictor/in/ref_traj_poses', 'traj_predictor/ref_traj_poses'),
             ('mpc_tracker/out/path', 'mpc_tracker/path'),
             ('mpc_tracker/command/trajectory', 'mpc_tracker/command/trajectory')
         ]
@@ -41,6 +50,7 @@ def generate_launch_description():
 
     return LaunchDescription([
         yaml_path_arg,
+        mpc_namespace_arg,
         mpc_node,
         LogInfo(msg=["Launching mpc_node with parameters from: ", LaunchConfiguration('yaml_path')]),
     ])
