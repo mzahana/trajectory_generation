a7ade6cb0e5529aaff1125be6ab3a0a30197667a
MohamedAbdelkader <mohamedashraf123@gmail.com>
2024-03-19 12:33:12 +0300

    Update launch/mpc_12state.launch.py

--- launch/mpc_12state.launch.py
+++ launch/mpc_12state.launch.py
@@ -23,12 +23,41 @@ def generate_launch_description():
         description='Path to the YAML file with parameters for the mpc_12state_node'
     )
 
-    # Declare the argument for the YAML file path
+    # Declare the argument for the node namespace
     mpc_namespace_arg = DeclareLaunchArgument(
         'mpc_namespace',
         default_value='',
         description='Namespace of the mpc_node'
     )
+
+    # MPC input odom topic
+    mpc_odom_topic_arg = DeclareLaunchArgument(
+        'mpc_odom_topic',
+        default_value='interceptor/mavros/local_position/odom',
+        description='MPC input odom topic topic'
+    )
+
+    # MPC input IMU topic
+    mpc_imu_topic_arg = DeclareLaunchArgument(
+        'mpc_imu_topic',
+        default_value='interceptor/mavros/imu/data',
+        description='MPC input IMU topic topic'
+    )
+
+    # MPC input reference path topic
+    mpc_ref_path_topic_arg = DeclareLaunchArgument(
+        'mpc_ref_path_topic',
+        default_value='/out/gru_predicted_path',
+        description='MPC input reference path topic'
+    )
+
+    # MPC output trajectory command topic
+    mpc_cmd_topic_arg = DeclareLaunchArgument(
+        'mpc_cmd_topic_arg',
+        default_value='/interceptor/geometric_controller/multi_dof_setpoint',
+        description='MPC output command topic'
+    )
+
 
     # Define the node
     mpc_node = Node(
@@ -39,14 +68,14 @@ def generate_launch_description():
         output='screen',
         parameters=[LaunchConfiguration('yaml_path')],
         remappings=[
-            ('mpc/in/odom', 'interceptor/mavros/local_position/odom'),
-            ('mpc/in/imu', 'interceptor/mavros/imu/data'),
+            ('mpc/in/odom', LaunchConfiguration('mpc_odom_topic')),
+            ('mpc/in/imu', LaunchConfiguration('mpc_imu_topic')),
             ('mpc/in/ref_traj', 'mpc/in/ref_traj'),
             ('mpc/in/ref_traj_poses', 'mpc/in/ref_traj_poses'),
             # ('mpc/in/ref_traj_path', 'traj_predictor/const_vel_path'),
-            ('mpc/in/ref_traj_path', '/out/gru_predicted_path'),
+            ('mpc/in/ref_traj_path', LaunchConfiguration('mpc_ref_path_topic')),
             ('mpc/out/path', 'mpc/out/path'),
-            ('mpc/out/trajectory_command', '/interceptor/geometric_controller/multi_dof_setpoint'),
+            ('mpc/out/trajectory_command', LaunchConfiguration('mpc_cmd_topic_arg')),
         ],
         # prefix='gdb -ex run --args'
     )
@@ -54,6 +83,10 @@ def generate_launch_description():
     return LaunchDescription([
         yaml_path_arg,
         mpc_namespace_arg,
+        mpc_odom_topic_arg,
+        mpc_imu_topic_arg,
+        mpc_ref_path_topic_arg,
+        mpc_cmd_topic_arg,
         mpc_node,
         LogInfo(msg=["Launching mpc_12state_node with parameters from: ", LaunchConfiguration('yaml_path')]),
     ])
